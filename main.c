3437f1e3a7ee4bf3df069e989c80e585e77a176e
forgottenx <shkurin_vanbka@mail.ru>
2017-05-11 02:35:15 +0700

    Implement data input and validate data

--- main.c
+++ main.c
@@ -4,10 +4,19 @@
 
 int main()
 {
-	int v,t;
-	printf("Enter the value\n");
-	scanf("%d",&v);
-	printf("Enter the time\n");
-	scanf("%d",&t);
- 	return 0;
+	int v, t;
+	printf("Enter the value (v >= 10000)\n");
+	while(1)
+	{
+		scanf("%d",&v);
+		if (v >= 10000) break;
+		printf("Wrong value! Try again\n");
+	}
+	printf("Enter the time (0 <= t <= 365)\n");
+	while(1)
+	{
+		scanf("%d",&t);
+		if (t >= 0 && t <= 365) break;
+		printf("Wrong time! Try again\n");
+	}
 }
