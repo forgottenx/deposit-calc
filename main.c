01df768817d2d3481532e02429fa5400e635d243
forgottenx <shkurin_vanbka@mail.ru>
2017-05-11 02:42:46 +0700

    Implement the calculation of the deposit

--- main.c
+++ main.c
@@ -4,19 +4,46 @@
 
 int main()
 {
-	int v, t;
-	printf("Enter the value (v >= 10000)\n");
-	while(1)
+	int t, timePer;
+	float v;
+ 	printf("Enter the value (v >= 10000)\n");
+ 	while(1)
+ 	{
+		scanf("%f", &v);
+ 		if (v >= 10000) break;
+ 		printf("Wrong value! Try again\n");
+ 	}
+ 	printf("Enter the time (0 <= t <= 365)\n");
+ 	while(1)
+ 	{
+		scanf("%d", &t);
+ 		if (t >= 0 && t <= 365) break;
+ 		printf("Wrong time! Try again\n");
+ 	}
+	if (t >= 0 && t <= 30) timePer=1;
+	if (t >= 31 && t <= 120) timePer=2;
+	if (t >= 121 && t <= 240) timePer=3;
+	if (t >= 241 && t <= 365) timePer=4;
+	if (v < 100000)
 	{
-		scanf("%d",&v);
-		if (v >= 10000) break;
-		printf("Wrong value! Try again\n");
+		switch(timePer)
+		{
+			case 1: v -= (((v/100) * 10)/ 365) * t; break;
+			case 2: v += (((v/100) * 2)/ 365) * t;  break;
+			case 3: v += (((v/100) * 6)/ 365) * t;  break;
+			case 4: v += (((v/100) * 12)/ 365) * t;  break;
+		}
 	}
-	printf("Enter the time (0 <= t <= 365)\n");
-	while(1)
+	else
 	{
-		scanf("%d",&t);
-		if (t >= 0 && t <= 365) break;
-		printf("Wrong time! Try again\n");
+		switch(timePer)
+		{
+			case 1: v -= (((v/100) * 10)/ 365) * t; break;
+			case 2: v += (((v/100) * 3)/ 365) * t;  break;
+			case 3: v += (((v/100) * 8)/ 365) * t;  break;
+			case 4: v += (((v/100) * 15)/ 365) * t;  break;
+		}
 	}
+	printf("Value=%.2f \n", v);
+ 	return 0;
 }
