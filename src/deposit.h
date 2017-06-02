19028ad31ca9b88bba8c44ac85f1391897349216
forgottenx <shkurin_vanbka@mail.ru>
2017-06-02 13:56:28 +0700

    Fix deposit.h

--- src/deposit.h
+++ src/deposit.h
@@ -1,6 +1,6 @@
 #ifndef VALUE
 #define VALUE
-float InputValue(double v);
+int InputValue(double v);
 int InputTime(int i);
-void TaskCalculations(int t, double v);
+double TaskCalculations(int t, double v);
 #endif 
